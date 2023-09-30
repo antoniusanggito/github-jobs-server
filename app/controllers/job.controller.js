3cc60ea30a4ee0475fe579228d4a91a472b67105
Antonius Anggito Arissaputro <antonius.anggito@ui.ac.id>
2023-10-01 01:41:28 +0700

    Add getAllJob query param

--- app/controllers/job.controller.js
+++ app/controllers/job.controller.js
@@ -4,18 +4,48 @@ const { successMessage, errorMessage, status } = require('../helpers/status');
 const { options } = require('./../helpers/options');
 
 exports.getAllJob = async (req, res) => {
+  const { description, location, full_time } = req.query;
+
+  let data;
   try {
     const response = await fetch(
       `${process.env.API_URL}/positions.json`,
       options
     );
-    const data = await response.json();
-    const ret = successMessage(data);
-    res.status(status.success).send(ret);
+    data = await response.json();
   } catch (error) {
     console.error(error.message);
     res.status(status.error).send(errorMessage(error.message));
+    return;
   }
+
+  let filtered = data;
+  if (description) {
+    filtered = filtered.filter((job) => {
+      return (
+        job.description.toLowerCase().indexOf(description.toLowerCase()) !== -1
+      );
+    });
+  }
+
+  if (location) {
+    filtered = filtered.filter((job) => {
+      return job.location.toLowerCase().indexOf(location.toLowerCase()) !== -1;
+    });
+  }
+
+  if (full_time) {
+    filtered = filtered.filter((job) => {
+      return full_time === 'true'
+        ? job.type === 'Full Time'
+        : full_time === 'false'
+        ? job.type !== 'Full Time'
+        : true;
+    });
+  }
+
+  const ret = successMessage(filtered);
+  res.status(status.success).send(ret);
 };
 
 exports.getDetailJob = async (req, res) => {
@@ -36,9 +66,9 @@ exports.getDetailJob = async (req, res) => {
 
   // validate data if empty
   if (Object.keys(data).length === 0) {
-    return res.status(status.notfound).send(errorMessage('Job not available'));
+    res.status(status.notfound).send(errorMessage('Job not available'));
   } else {
     const ret = successMessage(data);
-    return res.status(status.success).send(ret);
+    res.status(status.success).send(ret);
   }
 };
